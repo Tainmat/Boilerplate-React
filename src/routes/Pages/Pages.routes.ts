89203c469e70e3ce1bd6c6c25e4c04ddd398547c
Tainã Pino <tmartins.pino@gmail.com>
2024-02-06 16:57:24 -0300

    feat: add component Home

--- src/routes/Pages/Pages.routes.ts
+++ src/routes/Pages/Pages.routes.ts
@@ -2,11 +2,17 @@ import { lazy } from 'react'
 
 import { IRouteProps } from '../routes.interface'
 
-import { ROUTE_WELCOME } from './Pages.paths'
+import { ROUTE_HOME, ROUTE_WELCOME } from './Pages.paths'
 
 const Welcome = lazy(() =>
   import('@/pages/Welcome').then(module => ({
     default: module.Welcome
+  }))
+)
+
+const Home = lazy(() =>
+  import('@/pages/Home').then(module => ({
+    default: module.Home
   }))
 )
 
@@ -15,6 +21,16 @@ const mainRoutes: IRouteProps[] = [
     path: ROUTE_WELCOME,
     component: Welcome,
     isPrivate: false
+  },
+  {
+    path: ROUTE_HOME,
+    component: Home,
+    isPrivate: true
+  },
+  {
+    path: '/',
+    component: Home,
+    isPrivate: true
   }
 ]
 
