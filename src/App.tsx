ca059a39e9379988afd5ddfe94d81f0990b3c5d8
Tainã Pino <tmartins.pino@gmail.com>
2024-02-06 10:14:39 -0300

    add conventional commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
-import { texte } from '@/Pages/texte'
 
 export function App() {
   return (
     <BrowserRouter>
       <div>
-        <div>hello</div>
+        <div>
+          <div />
+        </div>
       </div>
     </BrowserRouter>
   )
